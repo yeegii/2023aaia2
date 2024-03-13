89dc0a4d2b8a8586caf61c09b4f0e3c1718d52f7
yeegii <yeegii@gmail.com>
2024-03-13 11:58:44 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;///這裡可以改一改,看他做幾次
+    while(a>0)
+    {
+        printf("a是%d\n",a); ///一直印
+        a--;///a就變小了
+    }///其實就是a-=
+}
---
+++ week04/week04-5.py
@@ -0,0 +1,8 @@
+#week04-5
+a = int(input())
+
+ans=1
+for i in range(1,a+1):
+	ans*=i
+	
+print(ans,end='')
