b2608f3686d605fed61a610140a46a763a3d0775
yeegii <yeegii@gmail.com>
2024-03-20 11:12:30 +0800

    week05

---
+++ week05/week05-2d.cpp
@@ -0,0 +1,13 @@
+///從week05-1.cpp複製過來
+///Settings-Editor (左邊第7個)Source Formatter
+///選JAVA (C的爸爸)
+///CodeBLocks右鍵跳出視窗,Format use AStyle
+
+
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++) {
+        printf("i是%d", i)
+    }
+}
